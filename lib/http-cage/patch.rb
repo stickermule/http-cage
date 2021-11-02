e5ff8326437e5485788837e8baaaa5ccd2c0ba32
meehow <michasko37@gmail.com>
2021-11-02 13:54:14 +0100

    Add ssl_timeout

--- lib/http-cage/patch.rb
+++ lib/http-cage/patch.rb
@@ -6,6 +6,7 @@ module HTTPCage
       super(*args, &block)
       self.open_timeout = HTTPCage.connection
       self.read_timeout = HTTPCage.request
+      self.ssl_timeout  = HTTPCage.ssl
     end
   end
 end
